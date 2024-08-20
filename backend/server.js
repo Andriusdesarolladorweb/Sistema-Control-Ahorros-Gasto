f7e1921bf5bfe069e905557421f8fe775f3dec73
Andriusdesarolladorweb <andriusrosacastro>
2024-08-20 19:26:36 -0400

    Actuazalizaciones general

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,13 @@
-import express from "express";
-import fetch from "node-fetch"; // Asegúrate de instalar node-fetch
+import express from 'express';
+import cors from 'cors';
+import db from './database/conexion';
+import entradaRoutes from './routes/routes'
+
 
 const app = express();
-const port = 3002;
+const port = 3006;
 
-app.use(express.json());
 
-app.get("/entradas", async (req, res) => {
-  try {
-    const response = await fetch("http://localhost:3001/entradas"); // URL de la API externa
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
-    }
-    const data = await response.json();
-    res.json(data);
-  } catch (error) {
-    console.error("Error fetching data:", error);
-    res.status(500).json({ error: "Failed to fetch data" });
-  }
-});
-
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
+app.listen(port, () =>{
+  console.log("corriendo bien");
+})
